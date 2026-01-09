3cdfe1ee0c724aa0a856e6569e1bbdc398cb12d2
AL <404notfoundcybersec@protonmail.com>
2026-01-09 03:12:47 -0500

    Create mitre.py

---
+++ mitre.py
@@ -0,0 +1,14 @@
+MITRE_MAP = {
+'SSH_BRUTE': {
+'tactic': 'Credential Access',
+'technique': 'T1110 – Brute Force'
+},
+'INSECURE_SERVICE': {
+'tactic': 'Initial Access',
+'technique': 'T1078 – Valid Accounts'
+},
+'FILE_TAMPERING': {
+'tactic': 'Defense Evasion',
+'technique': 'T1562 – Impair Defenses'
+}
+}
