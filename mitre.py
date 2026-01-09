5993739179866745456036fafc1c0e326c09a3fc
AL <404notfoundcybersec@protonmail.com>
2026-01-09 03:31:29 -0500

    Update mitre.py

--- mitre.py
+++ mitre.py
@@ -1,14 +1,16 @@
+# MITRE ATT&CK mapping used by SOC engine
+
 MITRE_MAP = {
-'SSH_BRUTE': {
-'tactic': 'Credential Access',
-'technique': 'T1110 – Brute Force'
-},
-'INSECURE_SERVICE': {
-'tactic': 'Initial Access',
-'technique': 'T1078 – Valid Accounts'
-},
-'FILE_TAMPERING': {
-'tactic': 'Defense Evasion',
-'technique': 'T1562 – Impair Defenses'
-}
+    'SSH_BRUTE': {
+        'tactic': 'Credential Access',
+        'technique': 'T1110 – Brute Force'
+    },
+    'INSECURE_SERVICE': {
+        'tactic': 'Initial Access',
+        'technique': 'T1078 – Valid Accounts'
+    },
+    'FILE_TAMPERING': {
+        'tactic': 'Defense Evasion',
+        'technique': 'T1562 – Impair Defenses'
+    }
 }
